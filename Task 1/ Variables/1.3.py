70585b1e10b9ebecd393b21df7a515f0bc93e42f
Moinak <111623431+Cyberninja30@users.noreply.github.com>
2024-04-28 12:34:19 +0530

    Delete Task 1/ Variables directory

--- Task 1/ Variables/1.3.py
+++
@@ -1,5 +0,0 @@
-p=float(input("Enter the Principal amount:")) 
-r=float(input("Enter the rate of interest:")) 
-t=float(input("Enter the time period:")) 
-si=(p*r*t)/100 
-print("Simple Interest:",si)
